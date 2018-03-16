7c1df9f88c567386d56f3e19b1a99cc9e6ee3e86
Haitang <litianpeng0624.163.com>
2018-03-16 11:42:21 +0800

    no message

---
+++ FirstReposityModel.h
@@ -0,0 +1,13 @@
+//
+//  FirstReposityModel.h
+//  homework
+//
+//  Created by Haitang on 2018/3/16.
+//  Copyright © 2018年 zyb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirstReposityModel : NSObject
+- (NSString *)getUserName;
+@end
---
+++ FirstReposityModel.m
@@ -0,0 +1,15 @@
+//
+//  FirstReposityModel.m
+//  homework
+//
+//  Created by Haitang on 2018/3/16.
+//  Copyright © 2018年 zyb. All rights reserved.
+//
+
+#import "FirstReposityModel.h"
+
+@implementation FirstReposityModel
+- (NSString *)getUserName{
+    return @"liming";
+}
+@end
